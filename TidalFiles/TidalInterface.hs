32bb30a45270a0d1528a7eac7d25be7cf9289daf
Dsm0 <willrinkoff@gmail.com>
2020-03-12 00:16:21 -0400

    consolidated  interfacing functions

--- TidalFiles/TidalInterface.hs
+++ TidalFiles/TidalInterface.hs
@@ -1,10 +1,19 @@
 module TidalInterface where
 
-import P5Render
-import P5Expressions
 import Sound.Tidal.Context
 import qualified Data.Map as Map_
 
+changeFunc' stream list = sendFunc' list
+  where toEvent' ws we ps pe v = Event (Sound.Tidal.Context.Context []) (Just $ Sound.Tidal.Context.Arc ws we) (Sound.Tidal.Context.Arc ps pe) v
+          -- where [ws',we',ps',pe'] = map toRational [ws,we,ps,pe]
+        makeFakeMap list_ = Map_.fromList list_
+        makeFuncHelp :: [(String,Value)] -> ControlPattern
+        makeFuncHelp y = Pattern $ fakeEvent (makeFakeMap y:: ControlMap)
+          where fakeEvent a notARealArgument = [(toEvent' 0 1 0 1) a]
+        makeFunc :: [(String,Value)] -> [ControlPattern]
+        makeFunc x = [makeFuncHelp x]
+        sendFunc' = mapM_ (streamFirst stream) . makeFunc
+-- 
 loadSynthDefs' stream path = changeFunc' stream list
    where list = [("scMessage",VS "loadSoundFileFolder"),("filePath",VS path)]
 
@@ -20,13 +29,13 @@ loadSoundFiles' stream path = changeFunc' stream list
 loadSoundFile' stream path = changeFunc' stream list
    where list = [("scMessage",VS "loadSoundFile"),("filePath",VS path)]
 
-freeAllSoundFiles' stream path = changeFunc' stream list
-   where list = [("scMessage",VS "freeAllSoundFiles"),("filePath",VS path)]
+freeAllSoundFiles' stream = changeFunc' stream list
+   where list = [("scMessage",VS "freeAllSoundFiles")]
 
-freeSoundFiles' stream names = changeFunc' stream list
-   where list = [("scMessage",VS "freeSoundFiles"),("filePath",VS names)]
+freeSoundFiles' stream name = changeFunc' stream list
+   where list = [("scMessage",VS "freeSoundFiles"),("filePath",VS name)]
 
-postSampleInfo' stream names = changeFunc' stream list
+postSampleInfo' stream = changeFunc' stream list
    where list = [("scMessage",VS "postSampleInfo")]
 
 initFreqSynthWindow' stream = changeFunc' stream
